3666a03527bf8145b3281af1395fe5fad1e68c4e
cmmasaba <mmasaba3142@gmail.com>
2022-10-27 19:50:50 +0300

    Full deployment

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Creates and distribues and archive to my web servers"""
+from datetime import datetime
+from fabric.api import *
+import os
+import shlex
+
+
+env.hosts = ['54.90.20.3', '54.160.65.217']
+env.user = "ubuntu"
+
+
+def deploy():
+    """creates and distributes archive"""
+    try:
+        archive_path = do_pack()
+    except Exception:
+        return False
+
+    return do_deploy(archive_path)
+
+
+def do_pack():
+    """generates tgz archive"""
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """deploys archive to servers"""
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
